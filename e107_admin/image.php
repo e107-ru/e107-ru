ffd2d4b356c4524d319d95359b2ad5118b96ab09
net1313 <net1313@gmail.com>
2018-08-24 22:42:26 +0200

    master

--- e107_admin/image.php
+++ e107_admin/image.php
@@ -1268,7 +1268,8 @@ class media_admin_ui extends e_admin_ui
 		}
 		else
 		{
-			echo $this->mediaSelectUpload();		
+			return $this->mediaManagerTabs();
+			//  echo $this->mediaSelectUpload();
 		}	
 		
 	}
@@ -1373,6 +1374,8 @@ class media_admin_ui extends e_admin_ui
 
 		$cat = $this->getQuery('for');
 
+		$cat = urldecode($cat);
+
 		$tabOptions = array(
 			'core-media-icon'   => array('caption'=> $tp->toGlyph('fa-file-photo-o').IMALAN_72,     'method' => 'iconTab' ),
 			'core-media-image'   => array('caption'=> $tp->toGlyph('fa-file-photo-o').ADLAN_105,    'method' => 'imageTab2' ),
@@ -1417,10 +1420,12 @@ class media_admin_ui extends e_admin_ui
 
 		$text = $frm->tabs($tabs, array('id'=>'admin-ui-media-manager', 'class'=>'media-manager'));
 
-		if($options['bbcode'] || E107_DEBUG_LEVEL > 0)
+	/*	if($options['bbcode'] || E107_DEBUG_LEVEL > 0)
 		{
+
+		}*/
+
 			$text .= $this->mediaManagerSaveButtons($options);
-		}
 
 		$text .= $this->mediaManagerPlaceholders();
 
@@ -1435,6 +1440,9 @@ class media_admin_ui extends e_admin_ui
 	 */
 	function mediaSelectUpload($type='image') 
 	{
+	//	return $this->mediaManagerTabs();
+
+
 		$frm = e107::getForm();
 		$tp = e107::getParser();
 
@@ -1449,7 +1457,7 @@ class media_admin_ui extends e_admin_ui
 
 		if(deftrue('e_DEBUG_MEDIAPICKER'))
 		{
-			return $this->mediaManagerTabs();
+		//	return $this->mediaManagerTabs();
 		}
 
 
@@ -1685,10 +1693,10 @@ class media_admin_ui extends e_admin_ui
 	private function mediaManagerPlaceholders()
 	{
 			$type = (E107_DEBUG_LEVEL > 0) ?  "text" : "hidden";
-		$br = (E107_DEBUG_LEVEL > 0) ?  "<br />" : "";
+		$br = (E107_DEBUG_LEVEL > 0) ?  "<br style='clear:both' />" : "";
 
 		$text = "
-		".$br."<input title='bbcode' type='{$type}' readonly='readonly' class='span11 col-md-11' id='bbcode_holder' name='bbcode_holder' value='' />
+		".$br."<input title='bbcode' type='{$type}' style=readonly='readonly' class='span11 col-md-11' id='bbcode_holder' name='bbcode_holder' value='' />
 		".$br."<input title='html/wysiwyg' type='{$type}' class='span11 col-md-11' readonly='readonly' id='html_holder' name='html_holder' value='' />
 		".$br."<input title='(preview) src' type='{$type}' class='span11 col-md-11' readonly='readonly' id='src' name='src' value='' />
 		".$br."<input title='path (saved to db)' type='{$type}' class='span11 col-md-11' readonly='readonly' id='path' name='path' value='' />
@@ -1701,10 +1709,20 @@ class media_admin_ui extends e_admin_ui
 
 	private function mediaManagerSaveButtons($options = array())
 	{
-		if(empty($options['bbcode']))
+		if(empty($options['bbcode'])) // media picker mode.
 		{
-			return null;
+			$text = "<div class='buttons-bar' style='display:none;text-align:right;padding-right:15px;'>
+			
+			<button id='etrigger-submit' style='display:none' type='submit' data-modal-submit-class='btn-danger' class=' submit e-media-select-file-none e-dialog-close' data-target-label='' data-bbcode='".$options['bbcode']."' data-target='".$this->getQuery('tagid')."' name='reset_value' value='reset'  >
+			<span>".LAN_CLEAR."</span>
+			</button>
+		
+			</div>";
+
+			return $text;
 		}
+
+		// bbcode/wysiwyg mode.
 
 		// hidden from view but used by javascript to re-create the button in the modal-footer.
 		// Tinymce will remove the 'display:none' when loaded.
@@ -1996,8 +2014,8 @@ class media_admin_ui extends e_admin_ui
 
 		);
 
-
-		$close = (!empty($this->getQuery('bbcode'))) ? false : true; // only close on 'select' when bbcodes are not in use.
+		$bbcode = $this->getQuery('bbcode');
+		$close = (!empty($bbcode)) ? false : true; // only close on 'select' when bbcodes are not in use.
 
 
 		$items = array();
@@ -2313,8 +2331,8 @@ class media_admin_ui extends e_admin_ui
 
 		}
 
-
-		$close = (!empty($this->getQuery('bbcode'))) ? false : true; // only close on 'select' when bbcodes are not in use.
+		$bbcode = $this->getQuery('bbcode');
+		$close = (!empty($bbcode)) ? false : true; // only close on 'select' when bbcodes are not in use.
 
 		if(!empty($data))
 		{
