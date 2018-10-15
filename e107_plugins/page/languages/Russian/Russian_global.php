b413977f2a261701644a73066fefb66de9fe9841
net1313 <net1313@gmail.com>
2018-10-16 01:32:51 +0200

    master

--- e107_plugins/page/languages/Russian/Russian_global.php
+++ e107_plugins/page/languages/Russian/Russian_global.php
@@ -1,3 +1,14 @@
 <?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Russian Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2018/10/15 23:45:46
+|
+|        $Author: e107-ru $
++---------------------------------------------------------------+
+*/
 
-define("LAN_PLUGIN_PAGE_BOCHAP",  "Поиск в Книге/Главе");
+define("LAN_PLUGIN_PAGE_BOCHAP", "Поиск в Книге/Главе");
+define("LAN_PLUGIN_PAGE_NAME", "Страницы");
