87b58f2566e28bba074ecda0b70d59d94be66a8d
net1313 <net1313@gmail.com>
2019-01-13 17:51:42 +0100

    master

--- e107_languages/Russian/lan_date.php
+++ e107_languages/Russian/lan_date.php
@@ -4,7 +4,7 @@
 |        e107 website content management system Russian Language File
 |        Released under the terms and conditions of the
 |        GNU General Public License (http://gnu.org).
-|        Last Modified: 2018/08/15 23:15:20
+|        Last Modified: 2019/01/13 16:47:14
 |
 |        $Author: net1313 https://e107-ru.com $
 +---------------------------------------------------------------+
@@ -18,16 +18,18 @@ define("LANDT_06", "минута");
 define("LANDT_07", "секунда");
 define("LANDT_01s", "лет");
 define("LANDT_02s", "месяцев");
-define("LANDT_03s", "недель");
+define("LANDT_03s", "недели");
 define("LANDT_04s", "дней");
 define("LANDT_05s", "час.");
 define("LANDT_06s", "минут");
 define("LANDT_07s", "сек.");
+
 define("LANDT_08", "мин.");
 define("LANDT_08s", "мин.");
-define("LANDT_09", "сек.");
+define("LANDT_09", "секунд");
 define("LANDT_09s", "сек.");
 define("LANDT_AGO", "назад");
+define("LANDT_IN", "");
 define("LANDT_10", "Только что");
 
 ?>
