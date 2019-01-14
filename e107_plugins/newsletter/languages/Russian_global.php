d118554b6feca8d402a8f15cc40c0fea5a4db638
net1313 <net1313@gmail.com>
2019-01-15 00:22:45 +0100

    master

--- e107_plugins/newsletter/languages/Russian_global.php
+++ e107_plugins/newsletter/languages/Russian_global.php
@@ -1,4 +1,4 @@
 <?php
 
-define("LAN_PLUGIN_NEWSLETTER_NAME", "Рассылки новостей");
-define("LAN_PLUGIN_NEWSLETTER_DESCRIPTION", "Быстрый и простой способ настроить и отправить рассылку новостей.");
+define("LAN_PLUGIN_NEWSLETTER_NAME", "Рассылка новостей");
+define("LAN_PLUGIN_NEWSLETTER_DESCRIPTION", "Быстрый и простой способ настроить рассылку новостей сайта.");
